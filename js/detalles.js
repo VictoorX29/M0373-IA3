67672e081a1411d38a134af5427e80a75ee74243
Victor Martinez PC <victormn29@gmail.com>
2026-04-17 01:12:11 +0200

    Actualizar la función Detalles para cargar datos de hoteles desde Supabase y mejorar la gestión de errores

--- js/detalles.js
+++ js/detalles.js
@@ -1,34 +1,50 @@
-import { hotels } from './data.js';
+import { supabase } from './supabase.js';
 const params = new URLSearchParams(window.location.search);
 const id = Number(params.get('id'));
-const hotel = hotels.find((h) => h.id === id);
-if (!hotel) {
-	document.querySelector('.divgrid-detalles').innerHTML =
-		`<p>No hay imágenes para mostrar porque no se ha seleccionado ningún hotel.</p>`;
-	document.querySelector('.h2-divinfoizq').textContent = 'No hotel';
-} else {
-	document.querySelector('.divgrid-img-detalles img').src = hotel.image;
-	document.querySelector('.h2-divinfoizq').textContent = hotel.name;
-	document.querySelector('#p-direccion').textContent = hotel.address;
-	document.querySelector('.p-calificacion-result').textContent =
-		`${hotel.rating} (${hotel.reviewsCount} Reviews)`;
-	document.querySelector('.p-infohotel-detalles').textContent =
-		hotel.overviewText;
-	const facilities = document.querySelector('.div-facilities-detalles');
-	let htmlF = ``;
-	for (const facility of hotel.topFacilities) {
-		htmlF += `
+async function Detalles() {
+	const { data: hotel, error } = await supabase
+		.from('hotels')
+		.select(
+			`
+			*,
+				hotel_facilities(
+				facilities(id, label)
+				),
+				hotel_explore_area(name, distance)
+			`,
+		)
+		.eq('id', id)
+		.single();
+	if (error || !hotel) {
+		document.querySelector('.divgrid-detalles').innerHTML =
+			`<p>No hay imágenes para mostrar porque no se ha seleccionado ningún hotel.</p>`;
+		document.querySelector('.divgrid-detalles').innerHTML +=
+			`<p>${!error.code === 'PGRST116' ? `Error: ${error.message}` : 'Selecciona un hotel desde resultados.'}</p>`;
+		document.querySelector('.h2-divinfoizq').textContent = 'No hotel';
+	} else {
+		document.querySelector('.divgrid-img-detalles img').src =
+			`https://kwzqtbwcsamesdbmjyvb.supabase.co/storage/v1/object/public/myDreamPlace/${hotel.image_url}`;
+		document.querySelector('.h2-divinfoizq').textContent = hotel.name;
+		document.querySelector('.p-calificacion-result').textContent =
+			`${hotel.rating} (${hotel.reviews_count} Reviews)`;
+		document.querySelector('#p-direccion').textContent = hotel.address;
+		document.querySelector('.p-infohotel-detalles').textContent =
+			hotel.overview_text;
+		const facilities = document.querySelector('.div-facilities-detalles');
+		let htmlF = ``;
+		for (const facility of hotel.hotel_facilities) {
+			htmlF += `
         <div class="div-facility-detalles">
-            <img src="../images/detalles/facilityicons/${facility.id}.svg" alt="Icono ${facility.id}">
-            <p class="p-facility-detalles">${facility.label}</p>
+            <img src="../images/detalles/facilityicons/${facility.facilities.id}.svg" alt="Icono ${facility.facilities.id}">
+            <p class="p-facility-detalles">${facility.facilities.label}</p>
         </div>
         `;
-	}
-	facilities.innerHTML = htmlF;
-	const areas = document.querySelector('.div-explore-area-detalles');
-	let htmlA = '';
-	for (const area of hotel.exploreArea) {
-		htmlA += `
+		}
+		facilities.innerHTML = htmlF;
+		const areas = document.querySelector('.div-explore-area-detalles');
+		let htmlA = '';
+		for (const area of hotel.hotel_explore_area) {
+			htmlA += `
         <div class="div-explore-detalles">
             <img
                 src="../images/detalles/icono-ubicacion.svg"
@@ -39,6 +55,8 @@ if (!hotel) {
             </div>
         </div>
         `;
+		}
+		areas.innerHTML = htmlA;
 	}
-	areas.innerHTML = htmlA;
 }
+Detalles();
