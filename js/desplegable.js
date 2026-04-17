f0adce6ccb91c62e24e87604789f80fd52ec7002
Victor Martinez PC-Clase <victormn29@gmail.com>
2026-04-17 14:46:01 +0200

    Agregar sección de bienvenida en index-home-2.html, mejorar la gestión del menú desplegable y eliminar sombra en el cuerpo

--- js/desplegable.js
+++ js/desplegable.js
@@ -1,5 +1,13 @@
 const iconoPerfil = document.querySelector('.header-icono-perfil');
 const desplegable = document.querySelector('.header-desplegable');
-iconoPerfil.addEventListener('click', function () {
+
+iconoPerfil.addEventListener('click', function (e) {
+	e.stopPropagation();
 	desplegable.classList.toggle('hidden');
+});
+
+document.addEventListener('click', function (e) {
+	if (!desplegable.contains(e.target)) {
+		desplegable.classList.add('hidden');
+	}
 });
