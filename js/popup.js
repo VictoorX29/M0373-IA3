cad8e221a7b4b190dc43f95049b7385ef71e6f1e
Victor Martinez PC-Clase <victormn29@gmail.com>
2026-04-21 19:22:38 +0200

    feat: mejorar la gestión del popup y ajustar estilos en la página de inicio

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const popup = document.querySelector('.sect-welcome-card');
 	if (infoPopup === 'true') {
 		popup.classList.remove('hidden');
+		document.body.style.overflow = 'hidden';
 		sessionStorage.setItem('popup', 'false');
 		document
 			.querySelector('.welcome-card')
@@ -11,12 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
 			});
 		document.addEventListener('click', () => {
 			popup.classList.add('hidden');
+			window.location.reload();
 		});
 		document
 			.querySelector('.button-welcome-card')
 			.addEventListener('click', function (e) {
 				e.preventDefault();
 				popup.classList.add('hidden');
+				window.location.reload();
 			});
 	}
 });
