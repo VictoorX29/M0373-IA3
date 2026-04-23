e53a52a3bb18f7903ae9b8dc5c9149a804afbcb6
Victor Martinez PC <victormn29@gmail.com>
2026-04-23 23:48:52 +0200

    feat: implementar carga y vista previa de foto de perfil en el dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,18 +2,57 @@ import { supabase } from './supabase.js';
 const {
 	data: { user },
 } = await supabase.auth.getUser();
-if (user) {
-	const usuario = await fotoUser();
-	const fotoUsuario = document.querySelector('.fotoPerfilDash');
-	if (usuario.avatar_url) {
-		fotoUsuario.src = usuario.avatar_url;
+async function consultaFotoUsuario() {
+	const { data } = await supabase.storage
+		.from('myDreamPlace')
+		.list('profiles', {
+			search: `${user.id}.jpg`,
+		});
+	if (data) {
+		consultaCambiarFoto(data);
 	}
-}
-async function fotoUser() {
-	const { data, error } = await supabase
-		.from('profiles')
-		.select('*')
-		.eq('id', user.id)
-		.single();
 	return data;
 }
+async function consultaCambiarFoto(data) {
+	const fotoUsuario = document.querySelector('.fotoPerfilDash');
+	if (data && data.length > 0) {
+		fotoUsuario.src = `https://kwzqtbwcsamesdbmjyvb.supabase.co/storage/v1/object/public/myDreamPlace/profiles/${user.id}.jpg?t=${Date.now()}`;
+	}
+}
+async function consultaSubirFoto(archivo) {
+	const { data, error } = await supabase.storage
+		.from('myDreamPlace')
+		.upload(`profiles/${user.id}.jpg`, archivo, {
+			upsert: true,
+		});
+	if (error) {
+		console.log(error.message);
+	} else {
+		const fotoUsuario = document.querySelector('.fotoPerfilDash');
+		fotoUsuario.src = `https://kwzqtbwcsamesdbmjyvb.supabase.co/storage/v1/object/public/myDreamPlace/profiles/${user.id}.jpg?t=${Date.now()}`;
+		window.location.reload();
+	}
+}
+const botonFoto = document.querySelector('#buttonEditFoto');
+const divInput = document.querySelector('.divFotoInput');
+botonFoto.addEventListener('click', () => previewFoto());
+function previewFoto() {
+	divInput.classList.remove('hidden');
+	const inputFoto = document.querySelector('#archivo');
+	inputFoto.addEventListener('change', () => {
+		if (inputFoto.files[0]) {
+			const divPreview = document.querySelector('.divPreviewFotoInput');
+			divPreview.classList.remove('hidden');
+			const imgPreview = document.querySelector('#imgPreviewFoto');
+			imgPreview.src = URL.createObjectURL(inputFoto.files[0]);
+			botonFoto.classList.remove('hidden');
+			botonFoto.textContent = 'Confirmar';
+			botonFoto.removeEventListener('click', previewFoto);
+			botonFoto.addEventListener('click', async () => {
+				await consultaSubirFoto(inputFoto.files[0]);
+			});
+		}
+	});
+}
+
+document.addEventListener('DOMContentLoaded', consultaFotoUsuario());
--- js/header.js
+++ js/header.js
@@ -4,19 +4,24 @@ const {
 } = await supabase.auth.getUser();
 if (user) {
 	document.querySelector('.header-iconos').classList.remove('hidden');
-	const usuario = await fotoUser();
-	const fotoUsuario = document.querySelector('.header-icono-perfil');
-	if (usuario.avatar_url) {
-		fotoUsuario.src = usuario.avatar_url;
-	}
+	await fotoUser();
 } else {
 	document.querySelector('.header-botones').classList.remove('hidden');
 }
 async function fotoUser() {
-	const { data, error } = await supabase
-		.from('profiles')
-		.select('*')
-		.eq('id', user.id)
-		.single();
+	const { data } = await supabase.storage
+		.from('myDreamPlace')
+		.list('profiles', {
+			search: `${user.id}.jpg`,
+		});
+	if (data) {
+		consultaCambiarFoto(data);
+	}
 	return data;
+}
+async function consultaCambiarFoto(data) {
+	const fotoUsuario = document.querySelector('.header-icono-perfil');
+	if (data && data.length > 0) {
+		fotoUsuario.src = `https://kwzqtbwcsamesdbmjyvb.supabase.co/storage/v1/object/public/myDreamPlace/profiles/${user.id}.jpg?t=${Date.now()}`;
+	}
 }
